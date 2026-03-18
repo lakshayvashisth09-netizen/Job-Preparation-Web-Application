65e1a94bf72fee7959c479d807e081f82654cccd
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-03-18 16:37:37 +0530

    Final cleanup: removed lightningcss completely

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  css: {
+    // Ye do lines lightningcss ki error ko khatam kar dengi
+    transformer: 'postcss',
+    minify: 'esbuild',
+  },
+  build: {
+    cssMinify: 'esbuild', // LightningCSS ki jagah esbuild use hoga jo safe hai
+  }
 })
